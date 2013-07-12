101d64f757fffc30c2edb6fdd20ab3bffb320b9f
Rakesh Cherian <rakeshcherian@gmail.com>
2013-07-12 13:00:52 +0000

    check-0

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
+function readFromFile(fileName){
+    return fs.readFileSync(fileName).toString;
+}
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Rakesh!');
+  response.send(readFromFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
