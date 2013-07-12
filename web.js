9d1524c10f2f1081ecea194ccaaf66415fadc4ec
Rakesh Cherian <rakeshcherian@gmail.com>
2013-07-12 13:17:20 +0000

    i.

--- web.js
+++ web.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 
-function fileContents {
-    return fs.readFileSync('index.html', "utf8");
-}
-
 function readFromFile(fileName){
-    return fs.readFileSync(fileName).toString;
+    return fs.readFileSync(fileName,"utf8");
 }
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fileContents());
+  response.send(readFileSync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
